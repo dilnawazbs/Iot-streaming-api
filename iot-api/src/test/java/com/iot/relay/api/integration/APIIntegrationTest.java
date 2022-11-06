7d6f3733f25eff3866589b0d283749855df5a477
Dilnawaz Sayyad <dilnawaz.sayyad@knowledge-values.com>
2022-11-06 15:30:59 +0100

    Update APIIntegrationTest.java

--- iot-api/src/test/java/com/iot/relay/api/integration/APIIntegrationTest.java
+++ iot-api/src/test/java/com/iot/relay/api/integration/APIIntegrationTest.java
@@ -1,162 +1,170 @@
 package com.iot.relay.api.integration;
-// package com.iot.relay.api.integration;
 
-// import java.math.BigDecimal;
-// import java.time.OffsetDateTime;
-// import java.util.ArrayList;
+import java.math.BigDecimal;
+import java.time.OffsetDateTime;
+import java.util.ArrayList;
 
-// import org.junit.jupiter.api.Assertions;
-// import org.junit.jupiter.api.Test;
-// import org.junit.jupiter.api.extension.ExtendWith;
-// import org.mockito.Mock;
-// import org.springframework.beans.factory.annotation.Autowired;
-// import org.springframework.boot.test.autoconfigure.data.mongo.AutoConfigureDataMongo;
-// import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
-// import org.springframework.boot.test.context.SpringBootTest;
-// import org.springframework.boot.test.web.client.TestRestTemplate;
-// import org.springframework.boot.web.server.LocalServerPort;
-// import org.springframework.http.HttpEntity;
-// import org.springframework.http.HttpMethod;
-// import org.springframework.http.HttpStatus;
-// import org.springframework.http.ResponseEntity;
-// import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
-// import org.springframework.security.core.userdetails.User;
-// import org.springframework.security.core.userdetails.UserDetails;
-// import org.springframework.web.util.UriComponentsBuilder;
-// import org.springframework.test.context.ActiveProfiles;
-// import org.springframework.test.context.junit.jupiter.SpringExtension;
-// import org.springframework.test.web.reactive.server.WebTestClient;
-// import org.springframework.util.LinkedMultiValueMap;
-// import org.springframework.util.MultiValueMap;
-// import com.iot.relay.api.IotApiApplication;
-// import com.iot.relay.api.config.JwtRequestFilter;
-// import com.iot.relay.api.config.JwtTokenUtil;
-// import com.iot.relay.api.request.QueryRequest;
-// import com.iot.relay.model.SensorDataEntity;
-// //import com.iot.relay.repo.SensorDataRepository;
-// import com.iot.relay.repo.SensorDataRepository;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.data.mongo.AutoConfigureDataMongo;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.boot.web.server.LocalServerPort;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.web.context.WebApplicationContext;
+import org.springframework.web.util.UriComponentsBuilder;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.springframework.util.LinkedMultiValueMap;
+import org.springframework.util.MultiValueMap;
 
-// @ExtendWith(SpringExtension.class)
-// @SpringBootTest(properties = {
-// 		"spring.profiles.active=test" }, classes = IotApiApplication.class, webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
-// @AutoConfigureDataMongo
-// //@ActiveProfiles(value = "test")
-// public class APIIntegrationTest {
+import com.iot.relay.api.ApiApplication;
+import com.iot.relay.api.config.JwtRequestFilter;
+import com.iot.relay.api.config.JwtTokenUtil;
+import com.iot.relay.api.request.QueryRequest;
+import com.iot.relay.model.model.SensorDataEntity;
+import com.iot.relay.model.repo.SensorDataRepository;
 
-// 	@LocalServerPort
-// 	private int port;
+@ExtendWith(SpringExtension.class)
+@SpringBootTest(classes = {ApiApplication.class
+}, webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@AutoConfigureDataMongo
+@ContextConfiguration(classes = ApiApplication.class)
+public class APIIntegrationTest {
 
-// 	@Autowired
-// 	TestRestTemplate restTemplate;
+	@LocalServerPort
+	private int port;
 
-// 	@Autowired
-// 	private SensorDataRepository sensorDataRepository;
+	@Autowired
+	TestRestTemplate restTemplate;
 
-// 	@Autowired
-// 	WebTestClient webTestClient;
+	@Autowired
+	private SensorDataRepository sensorDataRepository;
 
-// 	@Autowired
-// 	private JwtTokenUtil jwtTokenUtil;
+	@Autowired
+	private JwtTokenUtil jwtTokenUtil;
 	
-// 	@Autowired
-// 	private JwtRequestFilter jwtRequestFilter;
+	@MockBean
+    private JwtTokenUtil jwtUtil;
 
-// 	@Test
-// 	public void testMinWithClusterIdAndTypeSuccess() {
-// 		initTable(sensorDataRepository);
-// 		QueryRequest request = new QueryRequest();
-// 		request.setClusterId(1l);
-// 		request.setEventType("TEMPERATURE");
-// 		request.setStartDateTime("2021-12-23");
-// 		request.setEndDateTime("2023-12-23");
+    @Autowired
+    private JwtRequestFilter jwtRequestFilter;
 
-// 		final UserDetails userDetails = new User("dilnawaz", "password", new ArrayList<>());
-// 		var tokenString = "Bearer " + jwtTokenUtil.generateToken(userDetails);
-// 		MultiValueMap<String, String> headers = new LinkedMultiValueMap<>();
-//         headers.add("Content-Type", "application/json");
-//         headers.add("Authorization", tokenString);
-//         ResponseEntity<Object> entity = new TestRestTemplate().exchange(
-//         		createURLWithPort("/query/min/", request), HttpMethod.GET, new HttpEntity<Object>(headers),
-//                 Object.class);
-//         Assertions.assertEquals(HttpStatus.OK, entity.getStatusCode());
-//         Assertions.assertEquals(1, entity.getBody());
-// 	}
+    @MockBean
+	private PasswordEncoder passwordEncoder;
 
-// 	@Test
-// 	public void testMaxWithClusterIdAndTypeSuccess() {
-// 		initTable(sensorDataRepository);
-// 		QueryRequest request = new QueryRequest();
-// 		request.setClusterId(1l);
-// 		request.setEventType("TEMPERATURE");
-// 		request.setStartDateTime("2021-12-23");
-// 		request.setEndDateTime("2023-12-23");
+    @MockBean
+    AuthenticationManager authenticationManager;
 
-// 		final UserDetails userDetails = new User("dilnawaz", "password", new ArrayList<>());
-// 		var tokenString = "Bearer " + jwtTokenUtil.generateToken(userDetails);
-// 		MultiValueMap<String, String> headers = new LinkedMultiValueMap<>();
-//         headers.add("Content-Type", "application/json");
-//         headers.add("Authorization", tokenString);
-//         ResponseEntity<Object> entity = new TestRestTemplate().exchange(
-//         		createURLWithPort("/query/max/", request), HttpMethod.GET, new HttpEntity<Object>(headers),
-//                 Object.class);
-//         Assertions.assertEquals(HttpStatus.OK, entity.getStatusCode());
-//         Assertions.assertEquals(444444, entity.getBody());
-// 	}
+    @Autowired
+    private WebApplicationContext context;
+
+	@Test
+	public void testMinWithClusterIdAndTypeSuccess() {
+		initTable(sensorDataRepository);
+		QueryRequest request = new QueryRequest();
+		request.setClusterId(1l);
+		request.setEventType("TEMPERATURE");
+		request.setStartDateTime("2021-12-23");
+		request.setEndDateTime("2023-12-23");
+
+		final UserDetails userDetails = new User("dilnawaz", "password", new ArrayList<>());
+		var tokenString = "Bearer " + jwtTokenUtil.generateToken(userDetails);
+		MultiValueMap<String, String> headers = new LinkedMultiValueMap<>();
+        headers.add("Content-Type", "application/json");
+        headers.add("Authorization", tokenString);
+        ResponseEntity<Object> entity = new TestRestTemplate().exchange(
+        		createURLWithPort("/query/min/", request), HttpMethod.GET, new HttpEntity<Object>(headers),
+                Object.class);
+        Assertions.assertEquals(HttpStatus.OK, entity.getStatusCode());
+        Assertions.assertEquals(111111, entity.getBody());
+	}
+
+	@Test
+	public void testMaxWithClusterIdAndTypeSuccess() {
+		initTable(sensorDataRepository);
+		QueryRequest request = new QueryRequest();
+		request.setClusterId(1l);
+		request.setEventType("TEMPERATURE");
+		request.setStartDateTime("2021-12-23");
+		request.setEndDateTime("2023-12-23");
+
+		final UserDetails userDetails = new User("dilnawaz", "password", new ArrayList<>());
+		var tokenString = "Bearer " + jwtTokenUtil.generateToken(userDetails);
+		MultiValueMap<String, String> headers = new LinkedMultiValueMap<>();
+        headers.add("Content-Type", "application/json");
+        headers.add("Authorization", tokenString);
+        ResponseEntity<Object> entity = new TestRestTemplate().exchange(
+        		createURLWithPort("/query/max/", request), HttpMethod.GET, new HttpEntity<Object>(headers),
+                Object.class);
+        Assertions.assertEquals(HttpStatus.OK, entity.getStatusCode());
+        Assertions.assertEquals(444444, entity.getBody());
+	}
 	
-// 	@Test
-// 	public void testMedianWithClusterIdAndTypeSuccess() {
-// 		initTable(sensorDataRepository);
-// 		QueryRequest request = new QueryRequest();
-// 		request.setClusterId(1l);
-// 		request.setEventType("TEMPERATURE");
-// 		request.setStartDateTime("2021-12-23");
-// 		request.setEndDateTime("2023-12-23");
+	@Test
+	public void testMedianWithClusterIdAndTypeSuccess() {
+		initTable(sensorDataRepository);
+		QueryRequest request = new QueryRequest();
+		request.setClusterId(1l);
+		request.setEventType("TEMPERATURE");
+		request.setStartDateTime("2021-12-23");
+		request.setEndDateTime("2023-12-23");
 
-// 		final UserDetails userDetails = new User("dilnawaz", "password", new ArrayList<>());
-// 		var tokenString = "Bearer " + jwtTokenUtil.generateToken(userDetails);
-// 		MultiValueMap<String, String> headers = new LinkedMultiValueMap<>();
-//         headers.add("Content-Type", "application/json");
-//         headers.add("Authorization", tokenString);
-//         ResponseEntity<Object> entity = new TestRestTemplate().exchange(
-//         		createURLWithPort("/query/median/", request), HttpMethod.GET, new HttpEntity<Object>(headers),
-//                 Object.class);
-//         Assertions.assertEquals(HttpStatus.OK, entity.getStatusCode());
-//         Assertions.assertEquals(222222, entity.getBody());
-// 	}
+		final UserDetails userDetails = new User("dilnawaz", "password", new ArrayList<>());
+		var tokenString = "Bearer " + jwtTokenUtil.generateToken(userDetails);
+		MultiValueMap<String, String> headers = new LinkedMultiValueMap<>();
+        headers.add("Content-Type", "application/json");
+        headers.add("Authorization", tokenString);
+        ResponseEntity<Object> entity = new TestRestTemplate().exchange(
+        		createURLWithPort("/query/median/", request), HttpMethod.GET, new HttpEntity<Object>(headers),
+                Object.class);
+        Assertions.assertEquals(HttpStatus.OK, entity.getStatusCode());
+        Assertions.assertEquals(333333, entity.getBody());
+	}
 	
-// 	private String createURLWithPort(String uri, QueryRequest request) {
-// 		UriComponentsBuilder builder = UriComponentsBuilder.fromHttpUrl("http://localhost:" + port + uri)
-// 				.queryParam("startDateTime", request.getStartDateTime())
-// 				.queryParam("endDateTime", request.getEndDateTime()).queryParam("clusterId", request.getClusterId())
-// 				.queryParam("eventType", request.getEventType());
-// 		return builder.build().toUriString();
-// 	}
+	private String createURLWithPort(String uri, QueryRequest request) {
+		UriComponentsBuilder builder = UriComponentsBuilder.fromHttpUrl("http://localhost:" + port + uri)
+				.queryParam("startDateTime", request.getStartDateTime())
+				.queryParam("endDateTime", request.getEndDateTime()).queryParam("clusterId", request.getClusterId())
+				.queryParam("eventType", request.getEventType());
+		return builder.build().toUriString();
+	}
 
-// 	/**
-// 	 * Fill table with dummy data to query
-// 	 * 
-// 	 * @param sensorDataRepository repository where data needs to be stored
-// 	 */
-// 	public static void initTable(SensorDataRepository sensorDataRepository) {
-// 		SensorDataEntity entity1 = SensorDataEntity.builder().id(1l).clusterId(1l).type("TEMPERATURE")
-// 				.name("Living Room Temp").value(new BigDecimal("111111"))
-// 				.timestamp(OffsetDateTime.parse("2022-08-14T18:18:55.479998Z")).build();
-// 		sensorDataRepository.save(entity1);
+	/**
+	 * Fill table with dummy data to query
+	 * 
+	 * @param sensorDataRepository repository where data needs to be stored
+	 */
+	public static void initTable(SensorDataRepository sensorDataRepository) {
+		SensorDataEntity entity1 = SensorDataEntity.builder().id(1l).clusterId(1l).type("TEMPERATURE")
+				.name("Living Room Temp").value(new BigDecimal("111111"))
+				.timestamp(OffsetDateTime.parse("2022-08-14T18:18:55.479998Z")).build();
+		sensorDataRepository.save(entity1);
 
-// 		SensorDataEntity entity2 = SensorDataEntity.builder().id(1l).clusterId(1l).type("TEMPERATURE")
-// 				.name("Living Room Temp").value(new BigDecimal("222222"))
-// 				.timestamp(OffsetDateTime.parse("2022-08-14T18:18:57.479998Z")).build();
-// 		sensorDataRepository.save(entity2);
+		SensorDataEntity entity2 = SensorDataEntity.builder().id(1l).clusterId(1l).type("TEMPERATURE")
+				.name("Living Room Temp").value(new BigDecimal("222222"))
+				.timestamp(OffsetDateTime.parse("2022-08-14T18:18:57.479998Z")).build();
+		sensorDataRepository.save(entity2);
 
-// 		SensorDataEntity entity3 = SensorDataEntity.builder().id(1l).clusterId(1l).type("TEMPERATURE")
-// 				.name("Living Room Temp").value(new BigDecimal("333333"))
-// 				.timestamp(OffsetDateTime.parse("2022-08-14T18:18:58.479998Z")).build();
-// 		sensorDataRepository.save(entity3);
+		SensorDataEntity entity3 = SensorDataEntity.builder().id(1l).clusterId(1l).type("TEMPERATURE")
+				.name("Living Room Temp").value(new BigDecimal("333333"))
+				.timestamp(OffsetDateTime.parse("2022-08-14T18:18:58.479998Z")).build();
+		sensorDataRepository.save(entity3);
 
-// 		SensorDataEntity entity4 = SensorDataEntity.builder().id(1l).clusterId(1l).type("TEMPERATURE")
-// 				.name("Living Room Temp").value(new BigDecimal("444444"))
-// 				.timestamp(OffsetDateTime.parse("2022-08-14T18:18:59.479998Z")).build();
-// 		sensorDataRepository.save(entity4);
-// 	}
+		SensorDataEntity entity4 = SensorDataEntity.builder().id(1l).clusterId(1l).type("TEMPERATURE")
+				.name("Living Room Temp").value(new BigDecimal("444444"))
+				.timestamp(OffsetDateTime.parse("2022-08-14T18:18:59.479998Z")).build();
+		sensorDataRepository.save(entity4);
+	}
 
-// }
+}
